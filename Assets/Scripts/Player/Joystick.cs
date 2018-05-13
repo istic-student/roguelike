944c95e6c288e4267a06e2b96eb2f4897286d195
Lionel Jouin <lionel.jouin@outlook.com>
2018-05-14 00:12:48 +0200

    Player controller, pad, multiplayer & animations

---
+++ Assets/Scripts/Player/Joystick.cs
@@ -0,0 +1,19 @@
+﻿namespace Assets.Scripts.Player
+{
+    public class Joystick {
+
+        public readonly string Horizontal;
+        public readonly string Vertical;
+        public readonly string Action;
+        public readonly string Use;
+
+        public Joystick(int playerNumber)
+        {
+            Horizontal = "Horizontal_P" + playerNumber;
+            Vertical = "Vertical_P" + playerNumber;
+            Action = "Action_P" + playerNumber;
+            Use = "Use_P" + playerNumber;
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # roguelike
+
+
+https://trello.com/isticstudent
+
+https://github.com/istic-student/roguelike
+
+https://projetl3istic20172018.slack.com/messages/CAER9D37W/
+
+https://drive.google.com/drive/folders/10aP7FhrHG_gJ-YKKqGxbI6g_oWgnBPhk
