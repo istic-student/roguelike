fcc606b91882659e9ff3126d97a1906634cb2aae
Lionel Jouin <lionel.jouin@outlook.com>
2018-05-14 15:32:29 +0200

    player improvements : move, raycast

---
+++ Assets/Scripts/Player/PlayerInteraction.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Player
+{
+    public class PlayerInteraction : MonoBehaviour {
+        
+
+
+    }
+}
---
+++ Assets/Scripts/Player/PlayerRaycaster.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Player
+{
+    public class PlayerRaycaster : MonoBehaviour
+    {
+
+        public float RayDistance = 1.5f;
+
+        private PlayerController _playerController;
+
+        private void Start()
+        {
+            _playerController = GetComponent<PlayerController>();
+        }
+
+        private void Update()
+        {
+
+            var direction = GetVectorDirection();
+            var hit = Physics2D.Raycast(transform.position, direction, RayDistance);
+            if (hit.collider != null)
+            {
+                Debug.Log(hit.collider);
+            }
+
+            Debug.DrawRay(transform.position, direction * RayDistance, Color.green);
+
+        }
+
+        private Vector2 GetVectorDirection()
+        {
+            switch (_playerController.Direction)
+            {
+                case 0:
+                    return Vector2.up;
+                case 1:
+                    return Vector2.right;
+                case 2:
+                    return Vector2.down;
+                case 3:
+                    return Vector2.left;
+            }
+            return Vector2.up;
+        }
+
+    }
+}
