0f0bf02544f647501a7d29a73fd3af4408626e95
Emily Delorme <emilydelormefr@gmail.com>
2018-06-20 22:48:18 +0200

    Procedural Map v1

---
+++ Assets/Scripts/Environnement/Room.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Environnement
+{
+	public class Room {
+		public Vector2 gridPos;
+		public RoomType RoomType;
+		public bool doorTop, doorBot, doorLeft, doorRight;
+		public Room(Vector2 _gridPos, RoomType _RoomType){
+			gridPos = _gridPos;
+			RoomType = _RoomType;
+		}
+	}
+
+}
---
+++ Assets/Scripts/Environnement/RoomType.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Environnement
+{
+    public enum RoomType
+    {
+        SpawnRoom,
+        NormalRoom,
+        TreasureRoom,
+        BossRoom,
+        SecretRoom
+    }
+
+}
