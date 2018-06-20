e01f24252ab95793033931a3055e8a2d75aab718
Charles Delorme <charlesdelormefr@gmail.com>
2018-06-20 22:48:18 +0200

    Procedural Map v1

---
+++ Assets/Scripts/UtilityBehaviors.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UtilityBehaviors : MonoBehaviour {
+
+	void Update () {//reload scene, for testing purposes
+		if (Input.GetKeyDown("r")){
+			SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+		}
+	}
+}
