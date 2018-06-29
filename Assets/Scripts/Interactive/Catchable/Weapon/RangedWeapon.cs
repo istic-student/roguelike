2e93cb5931ae4f9fee11efd0da52dd72f7ef4b61
Emily Delorme <emilydelormefr@gmail.com>
2018-06-29 20:08:35 +0200

    Fix: Sprite rotation for projectiles

--- Assets/Scripts/Interactive/Catchable/Weapon/RangedWeapon.cs
+++ Assets/Scripts/Interactive/Catchable/Weapon/RangedWeapon.cs
@@ -21,9 +21,22 @@ namespace Assets.Scripts.Interactive.Catchable.Weapon
 
             var direction = GetComponentInParent<CharacterController>().VectorDirection;
             projectile.transform.position = transform.position + new Vector3(direction.x, direction.y);
+            switch(GetComponentInParent<CharacterController>().Direction) {
+                case 1:
+                    projectile.transform.eulerAngles =  new Vector3Int(0, 0, 270);
+                    break;
+                case 2:
+                    projectile.transform.eulerAngles =  new Vector3Int(0, 0, 180);
+                    break;
+                case 3:
+                    projectile.transform.eulerAngles =  new Vector3Int(0, 0, 90);
+                    break;
+                default:
+                    break;
+            }
             var rb = projectile.GetComponent<Rigidbody2D>();
             rb.AddForce(direction * ProjectileSpeed);
-        }
+        }             
 
     }
 }
